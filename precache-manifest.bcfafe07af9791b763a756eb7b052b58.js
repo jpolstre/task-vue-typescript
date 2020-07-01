e6458e8e8145aff1fa90e36cd30dc2f193a15430
jpolstre <jpolstre@gmail.com.com>
2020-07-01 18:15:02 -0400

    Updates

---
+++ precache-manifest.bcfafe07af9791b763a756eb7b052b58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a456fbd8dbfd7c0e1be",
+    "url": "/css/app.b344f610.css"
+  },
+  {
+    "revision": "bebc6a415e943fac73da9e1e8db9e518",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a456fbd8dbfd7c0e1be",
+    "url": "/js/app.0ba5af1a.js"
+  },
+  {
+    "revision": "c4a404fe656af3aa6543",
+    "url": "/js/chunk-vendors.1da6034d.js"
+  },
+  {
+    "revision": "82f97349633db50db75f4d64b2e0e84c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
